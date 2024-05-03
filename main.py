086d9c0d1fa951bd6bc19bd4c564bddb730e01ba
Larissa Bueno <167626069+Signoretto11@users.noreply.github.com>
2024-05-03 18:43:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+n1 = float(input("Digite um número: "))
+
+if n1 % 2 == 0:
+
+  print("Seu número é PAR")
+
+if n1 % 2 == 1:        #também pode ser escrito com else
+
+  print("Seu número é IMPAR")
